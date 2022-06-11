1ee92427d25ead8e85b3cbb6b62add6a15ec9668
test <test@test.test>
2022-06-11 13:00:21 +0300

    add server

---
+++ TcpServer/ClientEntity.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TcpServer
+{
+    public class ClientEntity
+    {
+        public TcpClient _client;
+
+        public string LastMessage { get; set; }
+
+        public ClientEntity(TcpClient client)
+        {
+            _client = client;
+        }
+
+        public void Process()
+        {
+            NetworkStream networkStream = null;
+
+            try
+            {
+                networkStream = _client.GetStream();
+                
+
+                byte[] data = new byte[64];
+
+                while (true)
+                {
+                    StringBuilder builder = new();
+                    int bytes = 0;
+                    do
+                    {
+                        bytes = networkStream.Read(data, 0, data.Length);
+                        builder.AppendLine(Encoding.Unicode.GetString(data, 0, data.Length));
+                    }
+                    while (networkStream.DataAvailable);
+
+                    LastMessage = builder.ToString();
+                }
+
+                
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                networkStream?.Close();
+                _client?.Close();
+            }
+        }
+    }
+}
---
+++ TcpServer/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace TcpServer
+{
+    class Program
+    {
+        private static TcpListener s_tcpListener;
+        private const int PORT = 6666;
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                s_tcpListener = new TcpListener(IPAddress.Parse("127.0.0.1"), PORT);
+                s_tcpListener.Start();
+
+                Console.WriteLine("Я вас слушаю");
+                while (true)
+                {
+                    TcpClient client = s_tcpListener.AcceptTcpClient();
+                    ClientEntity clientEntity = new(client);
+                    Thread clientThread = new(new ThreadStart(clientEntity.Process));
+                    clientThread.Start();
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
